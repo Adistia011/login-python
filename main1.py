15b9d33d62d97ad2ecfe66cee9219e7a8f9ff2b8
Muhammad Adistia Pratama <129607608+Adistia011@users.noreply.github.com>
2023-12-28 00:13:52 +0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+username = "Orang Biasa"
+password = "12345678"
+
+input_username = input("Username mu: ")
+input_password = input("Password mu: ")
+
+if input_username == username and input_password == password:
+    print("Ternyata Bisa Login!")
+else:
+    print("Kayaknya Kedua-dua nya Ada Yang Salah Isi")
